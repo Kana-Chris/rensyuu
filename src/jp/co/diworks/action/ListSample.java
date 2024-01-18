4ac1edda6a4b5685aa52ccced48e2ab61b15dc6a
Kana-Chris <nyapo797@gmail.com>
2024-01-18 20:26:46 +0900

    24.1.18 追加

--- src/jp/co/diworks/action/ListSample.java
+++ src/jp/co/diworks/action/ListSample.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ListSample {
   
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		
 		list.add("1");
@@ -13,10 +13,18 @@ public class ListSample {
 		list.add("4");
 		list.add("5");
 		
-		for(String s:list) {
+		
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i));
+		}
+		
+		for(String s: list) {
 			System.out.println(s);
 		}
 	}
+	
+	
+	
 	
 	
 }
